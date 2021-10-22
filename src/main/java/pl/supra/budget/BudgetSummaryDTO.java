e83d051f1242121cbc7015227679b360bae9307d
Mateusz Ludwin <mateusz.ludwin@gmail.com>
2021-10-22 12:01:07 +0200

    Add new class DTO

--- src/main/java/pl/supra/budget/BudgetSummaryDTO.java
+++ src/main/java/pl/supra/budget/BudgetSummaryDTO.java
@@ -1,4 +1,27 @@
 package pl.supra.budget;
 
+import java.math.BigDecimal;
+
 public class BudgetSummaryDTO {
+    private final BigDecimal incomes;
+    private final BigDecimal expenses;
+    private final BigDecimal balance;
+
+    public BudgetSummaryDTO(BigDecimal incomes, BigDecimal expenses, BigDecimal balance) {
+        this.incomes = incomes;
+        this.expenses = expenses;
+        this.balance = balance;
+    }
+
+    public BigDecimal getIncomes() {
+        return incomes;
+    }
+
+    public BigDecimal getExpenses() {
+        return expenses;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
 }
